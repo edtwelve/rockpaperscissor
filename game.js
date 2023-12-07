cc7dc7a13ba9bd9524c97a11e2e78fb399581235
Edwing Muñoz <eamc201168@gmail.com>
2023-12-07 13:26:56 -0500

    add function to play round of paper, scissors, rock

--- game.js
+++ game.js
@@ -3,3 +3,35 @@ function getComputerChoice () {
     let ind = Math.floor(Math.random() * 3);
     return options[ind];
 }
+
+function playRound(playerSelection,computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock";
+        } else if (computerSelection === "scissors") {
+            return "You Win! Rock beats Scissors";
+        } else {
+            return "TIED";
+        }
+    }
+    if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You lose! Scissors beat Paper";
+        } else if (computerSelection === "rock") {
+            return "You Win! Paper beats Rock";
+        } else {
+            return "TIED";
+        }
+    }
+    if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats Scissors";
+        } else if (computerSelection === "paper") {
+            return "You Win! Scissors beat Paper";
+        } else {
+            return "TIED";
+        }
+    }
+}
